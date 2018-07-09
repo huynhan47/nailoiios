67ea237c3f3f1d9e74235e8055030e28e13551f0
KennyNguyen88 <giatrangrua@gmail.com>
2018-07-09 14:34:15 +0700

    Initial Commit

---
+++ MultiCollect/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MultiCollect
+//
+//  Created by Posco ICT VN on 7/9/18.
+//  Copyright © 2018 Posco ICT VN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

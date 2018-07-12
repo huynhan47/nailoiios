4419f85872bd72e1300010ac1b5730d97c6cf08e
huynhan007 <huynhxuanan@posco.net>
2018-07-12 10:36:39 +0700

    zzz

--- MultiCollect/ViewController.swift
+++ MultiCollect/ViewController.swift
@@ -56,7 +56,7 @@ class ViewController: UIViewController , UICollectionViewDelegate,UICollectionVi
         
          if collectionView == self.target{
             print(indexPath);
-            puzzText.append("11");
+            puzzText.append("19");
             answerText[indexPath.row] = " ";
             target.reloadData();
             from.reloadData()

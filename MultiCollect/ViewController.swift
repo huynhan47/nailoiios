34ad226da8f2d1fd3676d1da332152b40f93027a
KennyNguyen88 <giatrangrua@gmail.com>
2018-07-12 09:29:55 +0700

    first commit

--- MultiCollect/ViewController.swift
+++ MultiCollect/ViewController.swift
@@ -14,7 +14,8 @@ class ViewController: UIViewController , UICollectionViewDelegate,UICollectionVi
     @IBOutlet weak var target: UICollectionView!
     @IBOutlet weak var from: UICollectionView!
     
-    var places = ["1","2","place3","place4","3"]
+    var puzzText = ["1","2","3","4","5"]
+    var answerText = ["1","2","3","4","5"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,13 +36,13 @@ class ViewController: UIViewController , UICollectionViewDelegate,UICollectionVi
         return 1;
     }
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return places.count;
+        return puzzText.count;
     }
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         if collectionView == self.target{
         let cell:targetCellUnit = collectionView.dequeueReusableCell(withReuseIdentifier: "targetCellUnit", for: indexPath) as! targetCellUnit
         cell.backgroundColor = UIColor.red;
-        cell.targetLabel.text = places[indexPath.row]
+        cell.targetLabel.text = puzzText[indexPath.row]
             return cell;
         }
         else{
@@ -53,10 +54,21 @@ class ViewController: UIViewController , UICollectionViewDelegate,UICollectionVi
     }
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         
-         if collectionView == self.from{
-            print(indexPath)
+         if collectionView == self.target{
+            print(indexPath);
+            puzzText.append("6");
+            answerText[indexPath.row] = " ";
+            target.reloadData();
+            from.reloadData()
         }
+         else{
+            answerText.append("6");
+            puzzText[indexPath.row] = " ";
+            target.reloadData();
+            from.reloadData();
+            
         }
+    }
     
 }
 

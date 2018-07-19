80c07710c1bb90e43930ce3b7b42bbda7abfa2b3
huynhan007 <huynhxuanan@posco.net>
2018-07-19 16:57:07 +0700

    commit auto layout

--- MultiCollect/ViewController.swift
+++ MultiCollect/ViewController.swift
@@ -13,12 +13,19 @@ class ViewController: UIViewController , UICollectionViewDelegate,UICollectionVi
 
     @IBOutlet weak var target: UICollectionView!
     @IBOutlet weak var from: UICollectionView!
+  
+    @IBOutlet weak var heightConst: NSLayoutConstraint!
+    
     
     var puzzText = ["a","r","3","4","6","6","7","8","a","r","3","4","6","6","7","8"]
     var orgPuzzText = ["a","r","3","4","6","6","7","8","a","r","3","4","6","6","7","8"]
     var answerText   = [" ", " ", " "," "," "," ", " "," ", " ", " "," "," "," ", " "," ", " "] as Array
     var mappingText = [1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8] as Array
     var currentIndex = 0 as Int?;
+    var height = CGFloat(3) ;
+    let max = UIScreen.main.bounds .width;
+    var spacing = CGFloat(3) ;
+    var size = 3 ;
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -29,9 +36,27 @@ class ViewController: UIViewController , UICollectionViewDelegate,UICollectionVi
         target.dataSource = self;
         // Do any additional setup after loading the view, typically from a nib.
         
-     
+        let numberOfItemsPerRow = 9;
+       let flowLayout = target.collectionViewLayout as! UICollectionViewFlowLayout
+        
+        spacing = max / 212;
+        let totalSpace = flowLayout.sectionInset.left
+            + flowLayout.sectionInset.right +
+             (spacing * CGFloat(numberOfItemsPerRow - 1))
+        size = Int((max - totalSpace) / CGFloat(numberOfItemsPerRow))
+        heightConst.constant = CGFloat(size*4)
+        target.reloadData();
+    
     }
-
+    override func viewWillAppear(_ animated: Bool) {
+    
+        //let  height =  target.collectionViewLayout.collectionViewContentSize.height;
+        //heightConst.constant = height
+        //print("height1")
+        //print(height)
+        target.reloadData();
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -40,9 +65,10 @@ class ViewController: UIViewController , UICollectionViewDelegate,UICollectionVi
         return 1;
     }
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        if collectionView == self.target{
+        
+        if collectionView == self.target
+        {
             return puzzText.count;
-            
         }
         else
             //if collectionView == self.from
@@ -83,8 +109,7 @@ class ViewController: UIViewController , UICollectionViewDelegate,UICollectionVi
             puzzText[Int(mappingText[indexPath.row])] =  answerText[indexPath.row]; // revert puzzle text
     
             answerText[indexPath.row] = " "; //delete answer text
-            target.reloadData();
-            from.reloadData()
+           
         }
          else if collectionView == self.from{
            
@@ -93,14 +118,35 @@ class ViewController: UIViewController , UICollectionViewDelegate,UICollectionVi
             puzzText[indexPath.row] = " "; // delete the puzzle text
             print(answerText[currentIndex!])
             currentIndex?+=1;
-            target.reloadData();
-            from.reloadData();
+            
+           
             
         }
+        let  height = collectionView.collectionViewLayout.collectionViewContentSize.height;
+        heightConst.constant = height
+        print("height")
+        print(height)
+        target.reloadData();
+        from.reloadData();
     }
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let width = UIScreen.main.bounds.width
-        return CGSize(width: (width - 10)/11, height: (width - 10)/11) // width & height are the same to make a square cell
+        
+
+        print("size");
+        print(size)
+        let flowLayout = collectionViewLayout as! UICollectionViewFlowLayout
+        flowLayout.minimumInteritemSpacing = spacing;
+        //print("height2")
+        //height  = CGFloat(size*3);
+        //print(height)
+ 
+        return CGSize(width: size, height: size)
+        
+        //flowLayout.collectionViewContentSize
+        //let horizontalSpacing = layout.scrollDirection == .vertical ? layout.minimumInteritemSpacing : layout.minimumLineSpacing
+        //let width = UIScreen.main.bounds.width
+        //print(CGSize(width: (width - 10)/11, height: (width - 10)/11))
+        //return CGSize(width: (width - 10)/11, height: (width - 10)/11) // width & height are the same to make a square cell
     }
     
 }
